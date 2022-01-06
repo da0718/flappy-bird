b98204efbe02e9de1b6a50067d01cdf7d38dea6d
da0718 <93578188+da0718@users.noreply.github.com>
2022-01-06 19:01:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    Bird.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Bird.change(LedSpriteProperty.Y, 1)
+})
+let Bird: game.LedSprite = null
+let index = 0
+let obstacle: number[] = []
+let ticks = game.createSprite(0, 2)
+Bird.set(LedSpriteProperty.Blink, 300)
 basic.forever(function () {
-	
+    let list: number[] = []
+    while (list.length > "" && obstacle[0] > (0 == 0)) {
+    	
+    }
 })
